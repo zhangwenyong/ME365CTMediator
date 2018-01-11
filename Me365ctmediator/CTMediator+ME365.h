ea923c95241718582fb9c910f812518d1d52d7b4
张文勇 <>
2018-01-11 15:22:44 +0800

    初始化

---
+++ Me365ctmediator/CTMediator+ME365.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+ME365.h
+//  CTMediator_365ME
+//
+//  Created by 张文勇 on 2018/1/11.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (ME365)
+
+@end
---
+++ Me365ctmediator/CTMediator+ME365.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+ME365.m
+//  CTMediator_365ME
+//
+//  Created by 张文勇 on 2018/1/11.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import "CTMediator+ME365.h"
+
+@implementation CTMediator (ME365)
+
+@end
